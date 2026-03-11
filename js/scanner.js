e6647f8102b82002911753c9d6fcd1b033c5cf10
aplicacionescher-wq <aplicacionescher@gmail.com>
2026-03-11 15:52:04 -0600

    scanner.js

--- js/scanner.js
+++ js/scanner.js
@@ -1,87 +1,45 @@
-import { db } from "./firebase.js";
-
-import {
-collection,
-query,
-where,
-getDocs,
-updateDoc
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-
 function onScanSuccess(decodedText){
 
-let data = JSON.parse(decodedText);
+let data=JSON.parse(decodedText);
 
-let ahora = Date.now();
+let ahora=Date.now();
 
-let expiracion = {
+let expiracion={
 
-visita:86400000,
-proveedor:43200000,
-uber:7200000,
-paqueteria:43200000
+visita:24*60*60*1000,
+proveedor:12*60*60*1000,
+uber:2*60*60*1000,
+paqueteria:12*60*60*1000
 
 };
 
-let resultado = document.getElementById("resultado");
+let resultado=document.getElementById("resultado");
 
-if(ahora - data.timestamp > expiracion[data.tipo]){
+if(ahora-data.timestamp>expiracion[data.tipo]){
 
-resultado.innerHTML="❌ QR EXPIRADO";
-
-resultado.style.background="red";
-
+resultado.innerHTML="QR EXPIRADO";
 return;
 
 }
-
-verificarUso(data);
-
-}
-
-async function verificarUso(data){
-
-let resultado = document.getElementById("resultado");
-
-const q = query(
-collection(db,"visitas"),
-where("token","==",data.token)
-);
-
-const querySnapshot = await getDocs(q);
-
-querySnapshot.forEach(async (doc)=>{
-
-let visita = doc.data();
-
-if(visita.usado){
-
-resultado.innerHTML="❌ QR YA UTILIZADO";
-
-resultado.style.background="red";
-
-return;
-
-}
-
-await updateDoc(doc.ref,{usado:true});
 
 resultado.innerHTML=
 
-"✅ ACCESO PERMITIDO<br>"+
-"Casa: "+visita.casa+"<br>"+
-"Visitante: "+visita.nombre+"<br>"+
-"Hora: "+visita.hora;
+"ACCESO PERMITIDO<br>"+
+"Casa:"+data.casa+"<br>"+
+"Visitante:"+data.nombre;
 
-resultado.style.background="green";
+if(data.identificacion){
 
-});
+resultado.innerHTML+=
+"<br><img src='"+data.identificacion+"' width='200'>";
 
 }
 
-const html5QrCode = new Html5QrcodeScanner(
+}
+
+const html5QrCode=new Html5QrcodeScanner(
 "reader",
-{ fps:10, qrbox:250 }
+{fps:10,qrbox:250}
 );
 
 html5QrCode.render(onScanSuccess);
