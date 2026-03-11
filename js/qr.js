d3960f0ff60fff0cf8710168ef7e611b1b7af389
aplicacionescher-wq <aplicacionescher@gmail.com>
2026-03-11 13:26:16 -0600

    qr.js

---
+++ js/qr.js
@@ -0,0 +1,41 @@
+import { db } from "./firebase.js";
+
+import {
+addDoc,
+collection
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+window.crearQR = async function(){
+
+let casa = localStorage.getItem("casa");
+
+let nombre = document.getElementById("nombre").value;
+
+let tipo = document.getElementById("tipo").value;
+
+let hora = Date.now();
+
+let data = {
+
+casa,
+nombre,
+tipo,
+hora
+
+};
+
+await addDoc(collection(db,"visitas"), data);
+
+let texto = JSON.stringify(data);
+
+document.getElementById("qr").innerHTML="";
+
+new QRCode(document.getElementById("qr"),{
+
+text:texto,
+width:200,
+height:200
+
+});
+
+}
