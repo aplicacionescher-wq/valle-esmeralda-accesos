ec4e2ecc98540348ace0164fa752cdfd86cdf3ff
aplicacionescher-wq <aplicacionescher@gmail.com>
2026-03-11 15:56:15 -0600

    firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -2,22 +2,17 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebas
 
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-analytics.js";
-
 const firebaseConfig = {
-apiKey: "AIzaSyCgK2hbNsL7kgUEQFFbuweiStbfbijKtX8",
+
+apiKey: "AIzaSy...",
 authDomain: "valle-esmeralda-accesos.firebaseapp.com",
 projectId: "valle-esmeralda-accesos",
-storageBucket: "valle-esmeralda-accesos.firebasestorage.app",
+storageBucket: "valle-esmeralda-accesos.appspot.com",
 messagingSenderId: "1075927284762",
-appId: "1:1075927284762:web:635b74230f88d8740e65a3",
-measurementId: "G-WCC09W0Y5W"
+appId: "1:1075927284762:web:xxxx"
+
 };
 
 const app = initializeApp(firebaseConfig);
 
-const db = getFirestore(app);
-
-const analytics = getAnalytics(app);
-
-export { db };
+export const db = getFirestore(app);
