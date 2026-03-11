c59cce997675c2d03768656e6c01a4a66ed97e8b
aplicacionescher-wq <aplicacionescher@gmail.com>
2026-03-11 14:41:00 -0600

    scanner.js

--- js/scanner.js
+++ js/scanner.js
@@ -15,23 +15,40 @@ paqueteria:43200000
 
 let resultado = document.getElementById("resultado");
 
-if(ahora - data.hora > expiracion[data.tipo]){
+if(ahora - data.timestamp > expiracion[data.tipo]){
 
 resultado.innerHTML="❌ QR EXPIRADO";
+
 resultado.style.background="red";
 
 return;
 
 }
 
-resultado.innerHTML="✅ ACCESO PERMITIDO<br>"+data.casa;
+resultado.innerHTML=
+
+"✅ ACCESO PERMITIDO<br>"+
+"Casa: "+data.casa+"<br>"+
+"Visitante: "+data.nombre+"<br>"+
+"Hora QR: "+data.hora;
+
+if(data.foto){
+
+resultado.innerHTML +=
+"<br><img src='"+data.foto+"' width='150'>";
+
+}
 
 resultado.style.background="green";
 
 }
 
 const html5QrCode = new Html5QrcodeScanner(
+
 "reader",
-{ fps:10, qrbox:250 });
+
+{ fps:10, qrbox:250 }
+
+);
 
 html5QrCode.render(onScanSuccess);
