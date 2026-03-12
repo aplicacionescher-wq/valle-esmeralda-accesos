24f0cfbdadf14fe093466649912cfad4a30b788b
aplicacionescher-wq <aplicacionescher@gmail.com>
2026-03-11 18:39:56 -0600

    firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,18 +1,21 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-
-apiKey: "AIzaSy...",
-authDomain: "valle-esmeralda-accesos.firebaseapp.com",
-projectId: "valle-esmeralda-accesos",
-storageBucket: "valle-esmeralda-accesos.appspot.com",
-messagingSenderId: "1075927284762",
-appId: "1:1075927284762:web:xxxx"
-
+  apiKey: "AIzaSyCgK2hbNsL7kgUEQFFbuweiStbfbijKtX8",
+  authDomain: "valle-esmeralda-accesos.firebaseapp.com",
+  projectId: "valle-esmeralda-accesos",
+  storageBucket: "valle-esmeralda-accesos.firebasestorage.app",
+  messagingSenderId: "1075927284762",
+  appId: "1:1075927284762:web:635b74230f88d8740e65a3",
+  measurementId: "G-WCC09W0Y5W"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
