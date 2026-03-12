be9e548165321322ce7a9df9bc11d6f3e4ff4d33
aplicacionescher-wq <aplicacionescher@gmail.com>
2026-03-11 19:00:48 -0600

    sw.js

--- js/sw.js
+++ js/sw.js
@@ -1,25 +1,30 @@
-const CACHE="accesos-cache";
+const CACHE = "accesos-v1"
 
-self.addEventListener("install",e=>{
+self.addEventListener("install", e => {
 
 e.waitUntil(
 
-caches.open(CACHE).then(cache=>{
+caches.open(CACHE).then(cache => {
 
-return cache.addAll(["./"]);
+return cache.addAll([
+
+"./",
+"./index.html"
+
+])
 
 })
 
-);
+)
 
-});
+})
 
-self.addEventListener("fetch",e=>{
+self.addEventListener("fetch", e => {
 
 e.respondWith(
 
-caches.match(e.request).then(res=>res||fetch(e.request))
+caches.match(e.request).then(res => res || fetch(e.request))
 
-);
+)
 
-});
+})
