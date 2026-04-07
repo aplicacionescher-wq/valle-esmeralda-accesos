4ed8fd897a3ad24a9b2b841cef48ac4eed06a756
aplicacionescher-wq <aplicacionescher@gmail.com>
2026-04-07 12:37:05 -0600

    Update login.js

--- login.js
+++ login.js
@@ -2,25 +2,31 @@ import { db } from "./firebase.js";
 import { collection, query, where, getDocs } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 export async function login() {
-    let usuario = document.getElementById("domicilio").value;
-    let pass = document.getElementById("password").value;
+    const usuario = document.getElementById("domicilio").value;
+    const pass = document.getElementById("password").value;
 
-    const q = query(collection(db, "usuarios"), where("domicilio", "==", usuario), where("password", "==", pass));
-    const querySnapshot = await getDocs(q);
+    if (!usuario || !pass) return alert("Ingresa datos");
 
-    if (querySnapshot.empty) {
-        alert("Usuario o contraseña incorrectos");
-        return;
+    try {
+        const q = query(collection(db, "usuarios"), where("domicilio", "==", usuario), where("password", "==", pass));
+        const querySnapshot = await getDocs(q);
+
+        if (querySnapshot.empty) {
+            alert("Usuario o contraseña incorrectos");
+            return;
+        }
+
+        querySnapshot.forEach((doc) => {
+            const data = doc.data();
+            localStorage.setItem("rol", data.rol);
+            localStorage.setItem("casa", data.domicilio);
+            localStorage.setItem("usuario", data.nombre || "Residente");
+
+            if (data.rol === "admin") window.location.href = "historial.html";
+            else if (data.rol === "residente") window.location.href = "dashboard.html";
+            else if (data.rol === "caseta") window.location.href = "escaner.html";
+        });
+    } catch (e) {
+        alert("Error de conexión");
     }
-
-    querySnapshot.forEach((doc) => {
-        let data = doc.data();
-        localStorage.setItem("rol", data.rol);
-        localStorage.setItem("casa", data.domicilio);
-        localStorage.setItem("usuario", data.nombre || "Residente"); // Fundamental para el QR
-
-        if (data.rol === "admin") window.location.href = "admin.html";
-        else if (data.rol === "residente") window.location.href = "dashboard.html";
-        else if (data.rol === "caseta") window.location.href = "escaner.html";
-    });
 }
